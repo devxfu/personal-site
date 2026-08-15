9baaa84229a233f14aa34a5de25c4fdcefc9de56
BitProgramer <afu25400@gmail.com>
2026-08-15 16:22:51 -0400

    Chore: Temp elements for debugging purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./index.css";
+import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
+import Hero from "./components/Hero.jsx";
+import Nav from "./components/Nav.jsx";
+import SocialRail from './components/SocialRail.jsx';
 
 export default function App() {
   return (
-    <div className="bg-slate-900 text-white p-8 rounded-lg text-2xl font-bold">
-      <h1 className="text-2xl font-semibold">Alex F.</h1>
-    </div>
+    <main className="min-h-screen bg-base font-sans text-text">
+      <Hero />
+      <SocialRail />
+      <Nav activeSection="about" sectionIds={["about","skills","projects","resume","contact"]} />
+      <About />
+      <Skills />
+    </main>
   );
 }
