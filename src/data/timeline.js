9a84cdbe01db4925dd156d5cbf74821344613a27
BitProgramer <153872908+devxfu@users.noreply.github.com>
2026-08-16 11:41:44 -0400

    Feat: Create outline of data structure of occupation timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,42 @@
+export const timeline = [
+  {
+    id: "placeholder 1",
+    title: "test Data1",
+    org: "test Data1",
+    start: "2024-06-01",
+    end: null,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    type: "work",
+  },
+  {
+    id: "placeholder 2",
+    title: "test Data2",
+    org: "test Data2",
+    start: "2024-09-01",
+    end: null,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+    type: "education",
+  },
+  {
+    id: "placeholder 3",
+    title: "test Data3",
+    org: "test Data3",
+    start: "2023-06-01",
+    end: "2023-08-31",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+    type: "work",
+  },
+  {
+    id: "placeholder 4",
+    title: "test Data4",
+    org: "test Data4",
+    start: "2020-09-01",
+    end: "2024-06-30",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium tincidunt mattis, gravida placerat vestibulum.",
+    type: "education",
+  },
+];
